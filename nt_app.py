61c1a5c5632861f7da787c05f15ed15c9e3db189
Kiwisuki <marius.arlauskas01.dev@gmail.com>
2022-09-16 22:08:16 +0300

    website addition

---
+++ nt_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template, request, session, json
+import logging
+import urllib3
+from prod import get_prediction
+urllib3.disable_warnings()
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    if request.method == 'POST':
+        url = request.form['url']
+        url = 'https://' + 'www.aruodas.lt/' + '1-' + url.split('1-')[1][0:7]
+        prediction, price = get_prediction(url)
+        return render_template('index.html',\
+             value = f'Paskaičiuota vertė: {prediction} €, skelbime nurodyta kaina: {price} €')
+    return render_template('index.html', value = 'Laukiama nuorodos..')
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
